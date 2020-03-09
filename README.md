5b1deb89a83140d177a5a7e4e182d75898bd7673
Rian1010 <rtegally1098@gmail.com>
2020-03-10 00:47:05 +0100

    Added a README.md file and worked on the routes to render the templates using Flask

---
+++ README.md
@@ -0,0 +1,25 @@
+## Connection Python to MongoDB
+- sudo pip3 install flask-pymongo
+- sudo pip3 install dnspython
+
+## How to Install Flask
+- sudo pip3 install flask
+
+## Deployment onto Heroku
+- Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+- Install Heroku
+brew tap heroku/brew && brew install heroku
+
+- Login to Heroku 
+heroku login
+
+## Deployment to Heroku
+$ heroku login
+$ git init
+$ heroku git:remote -a shopping-list-google
+$ git add .
+$ git commit -am "make it better"
+$ heroku git:remote -a shopping-list-google
+
--- app.py
+++ app.py
@@ -1,1 +1,30 @@
 import os
+from flask import Flask, render_template, session, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__, template_folder='template')
+app.config["MONGO_DBNAME"] = 'myFirstCluster'
+app.config["MONGO_URI"] = 'mongodb+srv://Rian:j4JWQ1Ntzc9u0U7m@myfirstcluster-ges2b.mongodb.net/shopping-list?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE'
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Hello ' + session['username']
+
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/shopping-list.html')
+def shopping_list():
+    return render_template('shopping-list.html')
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=os.environ.get('PORT'),
+        debug=True)

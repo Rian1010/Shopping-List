b895b2217d095a1fee91e3a64f2352621b75f5a9
Rian1010 <rtegally1098@gmail.com>
2020-03-24 16:38:18 +0100

    Trying to solve an error when inserting an item into the shopping-list

--- app.py
+++ app.py
@@ -9,31 +9,15 @@ app.config["MONGO_URI"] = 'mongodb+srv://Rian:j4JWQ1Ntzc9u0U7m@myfirstcluster-ge
 
 mongo = PyMongo(app)
 
-@app.route('/<email_id>', methods=["GET", "POST"])
-def index(email_id):
+
+@app.route('/', methods=["GET", "POST"])
+def index():
     if session['email']:
         itemName = mongo.db.itemName.find()
-        accounts = mongo.db.account.find({"_id": ObjectId(email_id)})
-        return render_template('index.html', itemName=itemName, accounts=accounts)
+        return render_template('index.html', itemName=itemName)
     return render_template('login.html')
 
-@app.route('/shoppingList/<email_id>', methods=["GET", "POST"])
-def shoppingList(email_id):
-    if 'email' in session:
-        itemName = mongo.db.itemName.find()
-        accounts = mongo.db.account.find({"_id": ObjectId(email_id)})
-        return render_template("shopping-list.html", itemName=itemName, accounts=accounts)
-    return render_template('login.html')
 
-    # ------------ First try ------------ #
-    # if request.method == "POST":
-    #     itemName = mongo.db.itemName.find()
-    #     return render_template('shopping-list.html', itemName=itemName)
-    # email = session.get('email-address')
-    # if "user" in session:
-    #         return render_template('login.html')
-
-# Used this link for assistance https://www.programcreek.com/python/example/58659/werkzeug.security.check_password_hash
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -60,8 +44,9 @@ def login():
 #             session['name'] = request.form['username']
 #             return redirect(url_for('index.html'))
 #     else:
-#         flash(f'Invalid username')         
+#         flash(f'Invalid username')
 #     return 'Invalid username or password'
+
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -80,10 +65,29 @@ def register():
 
     return render_template('register.html')
 
+
 @app.route('/logout')
 def logout():
     session.pop('email', None)
     return redirect('login')
+
+
+@app.route('/shoppingList', methods=["GET", "POST"])
+def shoppingList():
+    if 'email' in session:
+        itemName = mongo.db.itemName.find()
+        return render_template("shopping-list.html", itemName=itemName)
+    return render_template('login.html')
+
+    # ------------ First try ------------ #
+    # if request.method == "POST":
+    #     itemName = mongo.db.itemName.find()
+    #     return render_template('shopping-list.html', itemName=itemName)
+    # email = session.get('email-address')
+    # if "user" in session:
+    #         return render_template('login.html')
+
+# Used this link for assistance https://www.programcreek.com/python/example/58659/werkzeug.security.check_password_hash
 
 
 @app.route('/listItem')
@@ -91,71 +95,73 @@ def listItem():
     itemName = mongo.db.itemName.find()
     return render_template('index.html', itemName=itemName)
 
-@app.route('/addShoppingList')
-def addShoppingList():
-    listName = mongo.db.listName.find()
-    return render_template('addShoppingList.html', listName=listName)
 
 @app.route('/addItem')
 def addItem():
+    accounts = mongo.db.account.find()
     itemName = mongo.db.itemName.find()
-    return render_template('addItem.html', itemName=itemName)
+    return render_template('addItem.html', itemName=itemName, accounts=accounts)
+
 
 @app.route('/insertList', methods=['GET', 'POST'])
 def insertList():
     listName = mongo.db.listName
     listName.insert_one({
-            'liName': request.form.get('liName'),
+        'liName': request.form.get('liName'),
     },)
     return redirect(url_for('shoppingList'))
 
-@app.route('/insertItem', methods=['GET', 'POST'])
-def insertItem():
-    itemName = mongo.db.itemName
-    itemName.insert_one(request.form.to_dict())
-    return redirect(url_for('shoppingList'))
 
-@app.route('/updateListName/<list_id>', methods=['GET','POST'])
-def updateListName(list_id):
-    listName = mongo.db.listName
-    listName.update({'_id': ObjectId(list_id)},
-                    {
-            'liName': request.form.get('liName'),
+@app.route('/insertItem/<owner_id>', methods=['GET', 'POST'])
+def insertItem(owner_id):
+    itemName = mongo.db.itemName
+    itemName.insert_one({
+        'owner': session['email'],
+        'itName': request.form.get('itName'),
+        'amount': request.form.get('amount')
     })
     return redirect(url_for('shoppingList'))
+
 
 @app.route('/updateItemName/<item_id>', methods=['GET', 'POST'])
 def updateItemName(item_id):
     itemName = mongo.db.itemName
+    accounts = mongo.db.account.find()
     itemName.update({"_id": ObjectId(item_id)},
-    {
-            'itName': request.form.get('itName'),
-            'amount': request.form.get('amount')
-    },)
-    return redirect(url_for('shoppingList', itemName=itemName))
+                    {
+                    'owner': session['email'],
+                    'itName': request.form.get('itName'),
+                    'amount': request.form.get('amount')
+                    },)
+    return redirect(url_for('shoppingList', itemName=itemName, accounts=accounts))
+
 
 @app.route('/editList/<list_id>')
 def editList(list_id):
     listName = mongo.db.listName.find_one({"_id": ObjectId(list_id)})
     return render_template('editShoppingList.html', listName=listName)
 
+
 @app.route('/editItem/<item_id>')
 def editItem(item_id):
     itemName = mongo.db.itemName.find_one({"_id": ObjectId(item_id)})
     return render_template('editListItem.html', itemName=itemName)
+
 
 @app.route('/deleteList')
 def deleteList():
     mongo.db.itemName.remove()
     return redirect(url_for('shoppingList'))
 
+
 @app.route('/deleteItem/<item_id>')
 def deleteItem(item_id):
     mongo.db.itemName.remove({"_id": ObjectId(item_id)})
     return redirect(url_for('shoppingList'))
 
+
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
-        port=os.environ.get('PORT'),
-        debug=True)
+            port=os.environ.get('PORT'),
+            debug=True)
